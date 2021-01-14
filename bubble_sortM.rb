ea64a45de7dc2cdb974f19d269a9683fb05e8efb
diamond-nicholas <diamondnicholas154@gmail.com>
2021-01-14 16:42:29 +0100

    fix code for the bubble_sort_by

--- bubble_sortM.rb
+++ bubble_sortM.rb
@@ -14,3 +14,21 @@ array
 end
 a = [1, 9, 1, 6, 7, 9, 6, 8]
 p bubble_sort(a) 
+
+def bubble_sort_by(arr)
+ j = arr.length - 1
+
+ while j.positive?
+  (0...j).each do |i|
+   arr[i], arr[i+1] = arr[i+1], arr[i] if  yield(arr[i], arr[i + 1]).positive?
+  end
+  j-=1
+ end
+ arr
+end
+
+new_bubble_sort_by = bubble_sort_by(["hi","hello","hey"]) do |left,right|
+  left.length - right.length
+  end
+
+p new_bubble_sort_by
