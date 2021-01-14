69333c9624efc7a20a30514016de0c9e625c0a9c
diamond-nicholas <diamondnicholas154@gmail.com>
2021-01-14 23:12:49 +0100

    fix the linters

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,16 +1,17 @@
 def bubble_sort(array)
   n = array.length
-    loop do
-  swapped = false
+  loop do
+    swapped = false
+
     (n - 1).times do |i|
-        if array[i] > array[i + 1]
-    array[i], array[i + 1] = array[i + 1], array[i]
-    swapped = true
-        end
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
+        swapped = true
+      end
     end
-    break if !swapped
-    end    
-    array
+    break unless swapped
+  end
+  array
 end
 a = [1, 9, 1, 6, 7, 9, 6, 8]
 p bubble_sort(a)
@@ -20,14 +21,14 @@ def bubble_sort_by(arr)
 
   while j.positive?
     (0...j).each do |i|
-  arr[i], arr[i + 1] = arr[i + 1], arr[i] if yield(arr[i], arr[i + 1]).positive?
+      arr[i], arr[i + 1] = arr[i + 1], arr[i] if yield(arr[i], arr[i + 1]).positive?
+    end
+    j -= 1
   end
-  j -= 1
- end
- arr
+  arr
 end
 
-  new_bubble_sort_by = bubble_sort_by(%w['hi', 'hello', 'hey']) do |left, right|
+new_bubble_sort_by = bubble_sort_by(%w[hi hello hey]) do |left, right|
   left.length - right.length
-  end
+end
 p new_bubble_sort_by
